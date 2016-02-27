dd71df9e4c2268f70ca5d105302bef92fa141439
Kellan Childers <Kellan Childers@gmail.com>
2016-02-27 11:18:05 -0500

    Updated README & added ability to place tiles

--- model/boards/__init__.py
+++ model/boards/__init__.py
@@ -1,5 +1,5 @@
 from .board import board
-from .boardops import demo
+from .boardops import demo, place_tile
 
 __author__ = 'Kellan Childers'
 __all__ = ['board', 'boardops']
--- model/boards/boardops.py
+++ model/boards/boardops.py
@@ -1,4 +1,5 @@
 import model.plots as pl
+import model.dice as dc
 from .tile import Tile
 __author__ = 'Kellan Childers'
 
@@ -12,3 +13,22 @@ def demo(stdbrd):
         if value > 4096:
             value = 2
     return board_copy
+
+
+def place_tile(stdbrd):
+    """Place a tile randomly on the board.
+
+    :param stdbrd: the standard board
+    :return: the board
+    """
+    tile = Tile(2) if dc.roll(1, 6) >= 2 else Tile(4)
+    done = False
+    if None not in stdbrd:
+        raise IndexError("Could not place tile")
+    while not done:
+        x = dc.roll(1, 4) - 1
+        y = dc.roll(1, 4) - 1
+        if stdbrd[x][y] is None:
+            stdbrd[x][y] = tile
+            done = True
+    return stdbrd
